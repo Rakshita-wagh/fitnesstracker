e08855216f4988dd8bed5a33286adec8bef405bf
Rakshita-wagh <01fe21bcs280@kletech.ac.in>
2023-12-09 12:45:42 +0530

    models,routes

--- trackbuddy/trackbuddy-api/server.js
+++ trackbuddy/trackbuddy-api/server.js
@@ -1,13 +1,22 @@
-const express= require('express');
+const express =require('express');
 const cors =require('cors');
 require('dotenv').config();
+const mongoose =require('mongoose') //connection
+const url = process.env.ATLAS_URI
 
-const app=express();
-const port=process.env.PORT || 5000
+mongoose.connect(url,{useNewUrlParser:true})
+const connection =mongoose.connection
+connection.once('open',()=>{
+    console.log("Mongoose is connected")
+})
+const app =express();
+const port =process.env.PORT || 5000
 
-app.use(cors())
+
+app.use(cors()) //middleware
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`server is running on port : ${port}`);
-});
+
+app.listen(port,()=>{
+    console.log('server is running:${port}')
+})
