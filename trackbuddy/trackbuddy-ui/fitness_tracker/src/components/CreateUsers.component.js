6a2e1beb76bbd45aa3abec711cb568db63479b81
Rakshita-wagh <01fe21bcs280@kletech.ac.in>
2023-12-10 12:52:42 +0530

    added

--- trackbuddy/trackbuddy-ui/fitness_tracker/src/components/CreateUsers.component.js
+++ trackbuddy/trackbuddy-ui/fitness_tracker/src/components/CreateUsers.component.js
@@ -3,10 +3,11 @@ import './CreateUsers.component.css';
 
 const  CreateUsers = ()=>{
     return (
-       <div >
-       <input type='text' className='form' placeholder='Name'/> <br/>
-        <input type='number' className='form' placeholder='Age'/> <br/>
+       <div className='form'>
+      Name: <input type='text'  placeholder='Name'/> <br/> <br/>
+       Age: <input type='number'  placeholder='Age'/> <br/>
         <button className='button'>Submit</button>
+        <input type='date'/>
        </div>
     )
 }
