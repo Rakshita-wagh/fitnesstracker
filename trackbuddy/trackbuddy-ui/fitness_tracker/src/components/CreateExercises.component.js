36600cea92439f3882fb4b489f402e338a2a9b89
Rakshita-wagh <01fe21bcs280@kletech.ac.in>
2023-12-10 12:56:38 +0530

    changed

--- trackbuddy/trackbuddy-ui/fitness_tracker/src/components/CreateExercises.component.js
+++ trackbuddy/trackbuddy-ui/fitness_tracker/src/components/CreateExercises.component.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { addExercise, getUsers } from './Services/api';
+import { addExercise, getUsers } from '../Services/api';
 
 
 const  CreateExercises = ()=>{
